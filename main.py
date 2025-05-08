4efa1f2f7c30e0372dc37c73977b7df3d3669d05
sigmotoa <si.galvismotoa@gmail.com>
2025-05-08 07:46:41 -0500

    fixing files and add requirements

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, Form
+from operations import save_file
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/upload/")
+async def upload_image(
+    file: UploadFile = File(...),
+    save_to_supabase: bool = Form(False)
+):
 
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+    result = await save_file(file, save_to_supabase)
+    return result
---
+++ operations.py
@@ -0,0 +1,51 @@
+import os
+import uuid
+from fastapi import UploadFile
+from dotenv import load_dotenv
+import aiofiles
+from supabase import create_client
+
+load_dotenv()
+
+SUPABASE_URL = os.getenv("NEXT_PUBLIC_SUPABASE_URL")
+SUPABASE_KEY=os.getenv("NEXT_PUBLIC_SUPABASE_ANON_KEY")
+SUPABASE_BUCKET=os.getenv("SUPABASE_BUCKET")
+
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+async def upload_file(file: UploadFile, filename:str):
+    content = await file.read()
+    file_name = f"image/{filename}"
+    res = supabase.storage().from_(SUPABASE_BUCKET).upload(file_name, content,{"content-type":file.content_type})
+
+    if res.get("error"):
+        return {"error": res["error"]["message"]}
+
+    file_url = supabase.storage().from_(SUPABASE_BUCKET).get_public_url(filename)
+
+    return file_url
+
+
+async def save_file(file: UploadFile, to_supabase: bool):
+    if not file.content_type.startswith("image/"):
+        return {"error": "Solo se permiten imágenes"}
+
+    new_filename = f"{uuid.uuid4().hex}_{file.filename}"
+
+    if to_supabase:
+        return await upload_file(file, new_filename)
+    else:
+        return await save_to_local(file, new_filename)
+
+
+async def save_to_local(file: UploadFile, filename: str):
+
+    os.makedirs("uploads", exist_ok=True)
+    file_path = os.path.join("uploads", filename)
+
+    async with aiofiles.open(file_path, "wb") as out_file:
+        content = await file.read()
+        await out_file.write(content)
+
+    return {"filename": filename, "local_path": file_path}
